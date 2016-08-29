61fffe5a8bd825ea7a57f612d78b1edcda97bd95
Ben Allen <ben.allen2@gmail.com>
2016-08-29 19:11:24 -0400

    starbucks time finish the lines

--- Metro.js
+++ Metro.js
@@ -1,20 +1,126 @@
-var lineCode = ""
+var lineCode = "";
 var userLine = "";
 var lineColor = "";
+var direction = "";// trains N+E bound are 1, S + W bound are 2
+var userStation = "";
+var stationCode = "";
 
-//line arrays
-var redLine = ["Shady Grove", "Rockville", "Twinbrook", "White Flint", "Grovesnor-Strathmore", "Medical Center", "Bethesda", "Friendship Heights", "Tenleytown-AU", "Van Ness-UDC", "Cleveland Park", "Woodley Park", "Dupont Circle", "Farragut North", "Metro Center", "Gallery Place-China Town", "Judiciary Square", "Union Station", "NoMa-Galludet U", "Rhode Island Ave-Brentwood", "Brookland-CUA", "Fort Totten", "Takoma", "Silver Spring", "Forest Glen", "Wheaton", "Glenmont"];
-var orangeLine = ["Vienna", "Dunn Loring", "West Falls Church", "East Falls Church", "Ballston-MU", "Virginia Square-GMU", "Clarendon", "Court House", "Rosslyn", "Foggy Bottom-GWU", "Farragut West", "McPherson Square", "Metro Center", "Federal Triangle", "Smithsonian", "L'Enfant Plaza", "Federal Center SW", "Capitol South", "Eastern Market", "Potomac Ave", "Stadium-Armory", "Minnesota Ave", "Deanwood", "Cheverly", "Landover", "New Carrollton"];
-var blueLine = ["Franconia-Springfield", "Van Dorn Street", "King Street - Old Town", "Braddock Road", "Potomac Yard", "Ronald Reagan Washington National Airport", "Crystal City", "Pentagon City", "Pentagon", "Arlington Cemetary", "Rosslyn", "Foggy Bottom-GWU", "Farragut West", "McPherson Square", "Metro Center", "Federal Triangle", "Smithsonian", "L'Enfant Plaza", "Federal Center SW", "Capitol South", "Eastern Market", "Potomac Ave", "Stadium-Armory", "Benning Road", "Capitol Heights", "Addison Road", "Morgan Boulevard", "Largo Town Center"];
-var greenLine = ["Branch Ave", "Suitland", "Naylor Road", "Southern Ave", "Congress Heights", "Anacostia", "Navy Yard - Ballpark", "Waterfront", "L'Enfant Plaza", "Archives", "Gallery Place", "Mount Vernon Square", "Shaw-Howard University", "U Street", "Columbia Heights", "Georgia Ave-Petworth", "Fort Totten", "West Hyattsville", "Prince George's Plaza", "College Park-UMD", "Greenbelt"];
+//line arrays need to convert these to objects with Ids
+var redLine = [{"name":"Shady Grove", "id":"A15"},
+{"name":"Rockville", "id":"A14"},
+{"name":"Twinbrook", "id":"A13"},
+{"name":"White Flint", "id":"A12"},
+{"name":"Grovesnor-Strathmore", "id":"A11"},
+{"name":"Medical Center", "id":"A10"},
+{"name":"Bethesda", "id":"A09"},
+{"name":"Friendship Heights", "id":"A08"},
+{"name":"Tenleytown-AU", "id":"A07"},
+{"name":"Van Ness-UDC", "id":"A06"},
+{"name":"Cleveland Park", "id":"A05"},
+{"name":"Woodley Park", "id":"A04"},
+{"name":"Dupont Circle", "id":"A03"},
+{"name":"Farragut North", "id":"A02"},
+{"name":"Metro Center", "id":"A01"},
+{"name":"Gallery Place-China Town", "id":"B01"},
+{"name":"Judiciary Square", "id":"B02"},
+{"name":"Union Station", "id":"B03"},
+{"name":"NoMa-Galludet U", "id":"B35"},
+{"name":"Rhode Island Ave-Brentwood", "id":"B04"},
+{"name":"Brookland-CUA", "id":"B05"},
+{"name":"Fort Totten", "id":"B06"},
+{"name":"Takoma", "id":"B07"},
+{"name":"Silver Spring", "id":"B08"},
+{"name":"Forest Glen", "id":"B09"},
+{"name":"Wheaton", "id":"B10"},
+{"name":"Glenmont", "id":"B11"}
+];
+
+var orangeLine = [{"name":"Vienna","id":"K08"},
+{"name":"Dunn Loring","id":"K07"},
+{"name":"West Falls Church","id":"K06"},
+{"name":"East Falls Church","id":"K05"},
+{"name":"Ballston-MU","id":"K04"},
+{"name":"Virginia Square-GMU","id":"K03"},
+{"name":"Clarendon","id":"K02"},
+{"name":"Court House","id":"K01"},
+{"name":"Rosslyn","id":"C05"},
+{"name":"Foggy Bottom-GWU","id":"C04"},
+{"name":"Farragut West","id":"C03"},
+{"name":"McPherson Square","id":"C02"},
+{"name":"Metro Center","id":"C01"},
+{"name":"Federal Triangle","id":"D01"},
+{"name":"Smithsonian","id":"D02"},
+{"name":"L'Enfant Plaza","id":"D03"},
+{"name":"Federal Center SW","id":"D04"},
+{"name":"Capitol South","id":"D05"},
+{"name":"Eastern Market","id":"D06"},
+{"name":"Potomac Ave","id":"D07"},
+{"name":"Stadium-Armory","id":"D08"},
+{"name":"Minnesota Ave","id":"D09"},
+{"name":"Deanwood","id":"D10"},
+{"name":"Cheverly","id":"D11"},
+{"name":"Landover","id":"D12"},
+{"name":"New Carrollton","id":"D13"}];
+
+var blueLine = [{"name":"Franconia-Springfield", "id":"J03"},
+{"name":"Van Dorn Street", "id":"J02"},
+{"name":"King Street - Old Town", "id":"C13"},
+{"name":"Braddock Road", "id":"C12"},
+{"name":"Potomac Yard", "id":"C11"},
+{"name":"Ronald Reagan Washington National Airport", "id":"C10"},
+{"name":"Crystal City", "id":"C09"},
+{"name":"Pentagon City", "id":"C08"},
+{"name":"Pentagon", "id":"C07"},
+{"name":"Arlington Cemetary", "id":"C06"},
+{"name":"Rosslyn", "id":"C05"},
+{"name":"Foggy Bottom-GWU", "id":"C04"},
+{"name":"Farragut West", "id":"C03"},
+{"name":"McPherson Square", "id":"C02"},
+{"name":"Metro Center", "id":"C01"},
+{"name":"Federal Triangle", "id":"D01"},
+{"name":"Smithsonian", "id":"D02"},
+{"name":"L'Enfant Plaza", "id":"D03"},
+{"name":"Federal Center SW", "id":"D04"},
+{"name":"Capitol South", "id":"D05"},
+{"name":"Eastern Market", "id":"D06"},
+{"name":"Potomac Ave", "id":"D07"},
+{"name":"Stadium-Armory", "id":"D08"},
+{"name":"Benning Road", "id":"G01"},
+{"name":"Capitol Heights", "id":"G02"},
+{"name":"Addison Road", "id":"G03"},
+{"name":"Morgan Boulevard", "id":"G04"},
+{"name":"Largo Town Center", "id":"G05"}];
+
+var greenLine = [{"name":"Branch Ave", "id":"F11"},
+{"name":"Suitland", "id":"F10"},
+{"name":"Naylor Road", "id":"F09"},
+{"name":"Southern Ave", "id":"F08"},
+{"name":"Congress Heights", "id":"F07"},
+{"name":"Anacostia", "id":"F06"},
+{"name":"Navy Yard - Ballpark", "id":"F05"},
+{"name":"Waterfront", "id":"F04"},
+{"name":"L'Enfant Plaza", "id":"F03"},
+{"name":"Archives", "id":"F02"},
+{"name":"Gallery Place", "id":"F01"},
+{"name":"Mount Vernon Square", "id":"E01"},
+{"name":"Shaw-Howard University", "id":"E02"},
+{"name":"U Street", "id":"E03"},
+{"name":"Columbia Heights", "id":"E04"},
+{"name":"Georgia Ave-Petworth", "id":"E05"},
+{"name":"Fort Totten", "id":"E06"}, {"name":"West Hyattsville", "id":"E07"}, {"name":"Prince George's Plaza", "id":"E08"}, {"name":"College Park-UMD", "id":"E09"}, {"name":"Greenbelt", "id":"E10"}];
+
 var yellowLine = ["Huntington", "Eisenhower Ave", "King Street-Old Town", "Braddock Road", "Potomac Yard", "Ronald Reagan Washington National Airport", "Crystal City", "Pentagon City", "Pentagon", "L'Enfant Plaza", "Archives-Navy Memorial-Penn Quarter", "Gallery Place", "Mount Vernon Square", "Shaw-Howard University", "U Street", "Columbia Heights", "Georgia Ave-Petworth", "Fort Totten", "West Hyattsville", "Prince George's Plaza", "College Park-UMD", "Greenbelt"];
+
+
+
+
+
 var silverLine = ["Wiehle-Reston East", "Spring Hill", "Greensboro", "Tysons Corner", "McLean", "East Falls Church", "Ballston-MU", "Virginia Square-GMU", "Clarendon", "Court House", "Rosslyn", "Foggy Bottom-GWU", "Farragut West", "McPherson Square", "Metro Center", "Federal Triangle", "Smithsonian", "L'Enfant Plaza", "Federal Center SW", "Capitol South", "Eastern Market", "Potomac Ave", "Stadium-Armory", "Benning Road", "Capitol Heights", "Addison Road", "Morgan Boulevard", "Largo Town Center"]
 
 
 
 
-
-/*  setInterval(function(){
+/*
 	$(function() {
 		var params = {
 				"api_key": "abc3f2f368624a3b95358b442ceb43d5",
@@ -44,7 +150,7 @@ var silverLine = ["Wiehle-Reston East", "Spring Hill", "Greensboro", "Tysons Cor
 				alert("error");
 		});
 });
-}, 10000);*/
+*/
 
 
 function lineSelect () {
@@ -71,7 +177,7 @@ function lineSelect () {
 			userLine = silverLine;
 			break;
 			case "BL":
-			lineColor = "blue";
+			lineColor = "#3381FF";
 			userLine = blueLine;
 			break;
 			case "OR":
@@ -96,22 +202,35 @@ function openListMenu () {
 	document.getElementById("stationMenu").style.width = "370px";
 	document.getElementById("main").style.marginLeft = "370px";
 	document.getElementById("main").style.opacity = "0.3";
+	stationSelect();
+	}
 
+function stationSelect() {
+	$("li").click(function(){
+		userStation = $(this).html();
+		for (var i = 0; i < userLine.length; i++){
+			if (userLine[i] === userStation){
+				stationCode = redCode[i];
+				console.log(stationCode)
+			}
+
+		}
+		closeNav();
+	})
 }
 
 function closeNav () {
 	document.getElementById("stationMenu").style.width = "0";
-	document.getElementById("main").style.width = "0";
+	document.getElementById("main").style.marginLeft = "0";
 	document.getElementById("main").style.opacity = "1";
 	$("#stationMenu").hide();
 }
+
 
 function openDirection () {
 	document.getElementById("directionMenu").style.width = "370px";
 	document.getElementById("main").style.marginLeft = "370px";
 	document.getElementById("main").style.opacity = "0.3";
-
-
 }
 
 $(document).ready(function(){
@@ -119,5 +238,7 @@ $(document).ready(function(){
 	$("#stationMenu").hide();
 	$("#directionMenu").hide();
 	$("#main").hide();
+	console.log(redLine.length);
+	console.log(redCode.length);
 
 })
