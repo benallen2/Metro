5a18fadd69a155d37f3516b8592c439897ff07a9
Ben Allen <ben.allen2@gmail.com>
2016-08-31 17:19:29 -0400

    Need direction fixed still

--- Metro.js
+++ Metro.js
@@ -1,12 +1,16 @@
 var lineCode = "";
 var userLine = "";
 var lineColor = "";
+var userColor = "";
+var trainColor = "";
 var direction = "";// trains N+E bound are 1, S + W bound are 2
 var userStation = "";
 var stationCode = "";
 var prevStation1 = "";
 var prevStation2 = "";
 var prevStation3 = "";
+
+var lineImg = "";
 
 var stationDisplay = [];//obj for the stations involved in main display
 
@@ -137,12 +141,7 @@ var yellowLine = [{"name":"Huntington", "id":"C15"},
 {"name":"College Park-UMD", "id":"E09"},
 {"name":"Greenbelt", "id":"E10"}];
 
-var silverLine = [{"name":"Ashburn", "id":"N12"},
-{"name":"Loudoun Gateway", "id":"N11"},
-{"name":"Washington Dulles International Airport", "id":"N10"},
-{"name":"Innovation Center", "id":"N09"},
-{"name":"Herndon", "id":"N08"},
-{"name":"Reston Town Center", "id":"N07"},
+var silverLine = [
 {"name":"Wiehle-Reston East", "id":"N06"},
 {"name":"Spring Hill", "id":"N04"},
 {"name":"Greensboro", "id":"N03"},
@@ -203,6 +202,35 @@ function wait (){
 			$("#destination").html(data.Trains[0].DestinationName);
 			$("#carsnumber").html(data.Trains[0].Car + " Car Train")
 
+				switch (data.Trains[0].Line){
+					case "RD":
+					trainColor = "Red";
+					$("#traincolor").css("color", "#e51434");
+					break;
+					case "GR":
+					trainColor = "Green";
+					$("#traincolor").css("color", "#0eab4a");
+					break;
+					case "YL":
+					trainColor = "Yellow";
+					$("#traincolor").css("color", "#EDCE00");
+					break;
+					case "SV":
+					trainColor = "Silver";
+					$("#traincolor").css("color", "#9d9f9d");
+					break;
+					case "BL":
+					trainColor = "Blue";
+					$("#traincolor").css("color", "#3381FF");
+					break;
+					case "OR":
+					trainColor = "Orange";
+					$("#traincolor").css("color", "#f38512");
+					break;
+				}
+				$("#traincolor").html(trainColor);
+
+
 				console.log(data);
 
 		})
@@ -219,46 +247,65 @@ function wait (){
 function lineSelect () {
 	$("#lineModal").show();
 	$(".lineBtn").click(function(){
-		$("#main").show();
+
 		$("#lineModal").fadeOut(300);
 		lineCode = $(this).attr("id");
 
 		switch (lineCode){
 			case "RD":
-			lineColor = "red";
+			lineColor = "#e51434";
 			userLine = redLine;
+			lineImg = "../Metro/Redline/RedLineFull.png";
+			userColor = "Red";
 			break;
 			case "GR":
-			lineColor = "green";
+			lineColor = "#0eab4a";
 			userLine = greenLine;
+			lineImg = "../Metro/Greenline/GreenLineFull.png";
+			userColor = "Green";
 			break;
 			case "YL":
 			lineColor = "#EDCE00";
 			userLine = yellowLine;
+			lineImg = "../Metro/Yellowline/YellowLineFull.png";
+			userColor = "Yellow";
 			break;
 			case "SV":
-			lineColor = "grey";
+			lineColor = "#9d9f9d";
 			userLine = silverLine;
+			lineImg = "../Metro/Silverline/SilverLineFull.png";
+			userColor = "Silver";
 			break;
 			case "BL":
 			lineColor = "#3381FF";
 			userLine = blueLine;
+			lineImg = "../Metro/Blueline/BlueLineFull.png";
+			userColor = "Blue";
 			break;
 			case "OR":
-			lineColor = "#FF7C0A";
+			lineColor = "#f38512";
 			userLine = orangeLine;
+			lineImg = "../Metro/Orangeline/OrangeLineFull.png";
+			userColor = "Orange";
 			break;
 			default:
 			lineColor = "red";
 			userLine = redLine;
+			lineImg = "../Metro/Images/ColorMetro-1.png";
+			userColor = "Red";
 			break;
 		}//switch for setting line color
+		$("#arriveTime").css("color", lineColor);
+		$("#destination").css("color", lineColor);
+		$("#carsnumber").css("color", lineColor);
 			$(".linebar").css("background-color", lineColor);
 			for (var i = 0; i < userLine.length; i++){
 				$("#stationList").append("<li>" + userLine[i].name + "</li>");//creating the menu station list with names
 				$("#stationList").css("color", lineColor);
 			}
 			openListMenu();
+			$("#lineGraphic").append("<img src=" + lineImg + " id='lineImg' >");
+
 	});
 }
 
@@ -270,7 +317,7 @@ function openListMenu () {//displays the list of station names
 	stationSelect();
 	}
 
-function stationSelect() {//what to do when a staion is clicked
+function stationSelect() {//what to do when a staion is clicked***********THERE ARE ERRORS HERE***************
 	$("li").click(function(){
 		userStation = $(this).html();
 		for (var i = 0; i < userLine.length; i++){
@@ -309,11 +356,13 @@ function openDirection () {//switches menu to direction list with end points.  W
 		$("#directionMenu").hide();
 			if ($(this).html() == userLine[0].name){
 				stationDisplay.splice(0, 3);
-				console.log(stationDisplay);
+				console.log(direction);
 				$("#stationLabel1").html(stationDisplay[0]);
 				$("#stationLabel2").html(stationDisplay[1]);
 				$("#stationLabel3").html(stationDisplay[2]);
 				$("#stationLabel4").html(stationDisplay[3]);
+				$("#stationLabel1").css("border", "solid " + lineColor + " 1px");
+				$("#stationLabel1").css("border-radius", "20px");
 			}
 			else {
 				stationDisplay.splice(4, 3);
@@ -321,8 +370,13 @@ function openDirection () {//switches menu to direction list with end points.  W
 				$("#stationLabel2").html(stationDisplay[1]);
 				$("#stationLabel3").html(stationDisplay[2]);
 				$("#stationLabel4").html(stationDisplay[3]);
-				console.log(stationDisplay);
+				$("#stationLabel4").css("border", "solid " + lineColor + " 1px");
+				$("#stationLabel4").css("border-radius", "20px");
+				console.log(direction);
 			}
+
+			$("#lineContainer").hide();
+			$("#main").show();
 			wait();
 	})
 }
@@ -332,10 +386,14 @@ $(document).ready(function(){
 	$("#stationMenu").hide();
 	$("#directionMenu").hide();
 	$("#main").hide();
-	$("#lineContainer").hide();
+	/*var search = "Takoma";
+	var index = -1;
+	for (var i=0; i < redLine.length; i++){
+		if (redLine[i].name === search){
+			index = i;
+			console.log(index);
+			break;
+		}
+	} THIs might be useful for direction in finding if destination index is higher or lower than userStation index?*/
 
-})
-
-
-
-//during menu selects, make the main window the image of the line. Duh.
+});
